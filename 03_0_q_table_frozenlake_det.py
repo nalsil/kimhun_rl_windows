1cd7cbd43287d52e6f02550031c8ddc42db023be
nalsil <nalsil@gmail.com>
2017-02-05 00:42:43 +0900

    ◦Lab 3: Dummy Q-learning (table)

---
+++ 03_0_q_table_frozenlake_det.py
@@ -0,0 +1,56 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+from gym.envs.registration import register
+import random as pr
+
+def rargmax(vector):    # https://gist.github.com/stober/1943451
+    """ Argmax that chooses randomly among eligible maximum idices. """
+    m = np.amax(vector)
+    indices = np.nonzero(vector == m)[0]
+    return pr.choice(indices)
+
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name' : '4x4', 'is_slippery': False}
+)
+env = gym.make('FrozenLake-v3')
+
+# Initialize table with all zeros
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+# Set learning parameters
+num_episodes = 2000
+
+# create lists to contain total rewards and steps per episode
+rList = []
+for i in range(num_episodes):
+    # Reset environment and get first new observation
+    state = env.reset()
+    rAll = 0
+    done = False
+
+    # The Q-Table learning algorithm
+    while not done:
+        action = rargmax(Q[state, :])
+
+        # Get new state and reward from environment
+        new_state, reward, done, _ = env.step(action)
+
+        # Update Q-Table with new knowledge using learning rate
+        Q[state, action] = reward + np.max(Q[new_state, :])
+
+        rAll += reward
+        state = new_state
+    rList.append(rAll)
+
+print("Success rate: " + str(sum(rList) / num_episodes))
+print("Final Q-Table Values")
+print("LEFT DOWN RIGHT UP")
+print(Q)
+
+error_config = {'ecolor': '0.3'}
+plt.bar(range(len(rList)), rList, color="blue")
+#plt.bar(range(len(rList)), rList, color='b', alpha=0.4)
+plt.show()
+
