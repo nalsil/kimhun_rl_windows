1c3f753d298e31b94ab378f25ff37b218252a3b4
nalsil <nalsil@gmail.com>
2017-02-07 13:11:28 +0900

    Lecture 2: OpenAI GYM 게임해보(윈도우 버전)

---
+++ 01_play_frozenlake_det_windows.py
@@ -0,0 +1,32 @@
+import gym
+from gym.envs.registration import register
+from colorama import init
+from kbhit import KBHit
+
+init(autoreset=True)    # Reset the terminal mode to display ansi color
+
+register(
+    id='FrozenLake-v3',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name' : '4x4', 'is_slippery': False}
+)
+
+env = gym.make('FrozenLake-v3')
+env.render()
+
+key = KBHit()
+
+while True:
+
+    action = key.getarrow();
+    if action not in [0, 1, 2, 3]:
+        print("Game aborted!")
+        break
+
+    state, reward, done, info = env.step(action)
+    env.render()
+    print("State: ", state, "Action: ", action, "Reward: ", reward, "Info: ", info)
+
+    if done:
+        print("Finished with reward", reward)
+        break
